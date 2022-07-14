e55c6181c640938a6efcaa1de40e46d80c191ec9
Bjorn Vanmarsenille <bjorn.vanmarsenille90@gmail.com>
2022-07-14 14:05:17 +0200

    Added a map from Leaflet.js and added a marker icon

--- ISS.js
+++ ISS.js
@@ -1,13 +1,35 @@
+
+// Laking a map and tiles
+const map = L.map('iss-map').setView([0, 0], 3);
+const attribution = '&copy; <a href="https://www.openstreetmap.org/coopyright">OpenStreetMap</a> contributors';
+
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer(tileUrl, { attribution });
+tiles.addTo(map);
 const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
 
+// Making a marker with a custom icon
+const myIcon = L.icon({
+    iconUrl: 'Images/International_Space_Station.svg.png',
+    iconSize: [100, 50],
+    iconAnchor: [25, 16],
+    popupAnchor: [-3, -76],
+});
+const marker = L.marker([0, 0], {icon: myIcon}).addTo(map);
+
+// get the data from the API
 async function getISS() {
     const response = await fetch(api_url);
     const data = await response.json();
     const { latitude, longitude } = data; // destructuring: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+    
+    // L.marker([latitude, longitude]).addTo(map);
+    marker.setLatLng([latitude, longitude]);
+    
     document.querySelector('#lat').textContent = latitude;
     document.querySelector('#lon').textContent = longitude;
     console.log(latitude);
     console.log(longitude);
 }
-
 getISS();
+
