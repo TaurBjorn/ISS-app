7bf9977e7f2d4be0ebff01008f4039d18113725b
Bjorn Vanmarsenille <bjorn.vanmarsenille90@gmail.com>
2022-07-14 14:58:45 +0200

    added new data from API: data, altitude and velocity

--- ISS.js
+++ ISS.js
@@ -21,13 +21,16 @@ const marker = L.marker([0, 0], {icon: myIcon}).addTo(map);
 async function getISS() {
     const response = await fetch(api_url);
     const data = await response.json();
-    const { latitude, longitude } = data; // destructuring: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+    const { latitude, longitude, altitude, velocity, timestamp } = data; // destructuring: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
     
     
     marker.setLatLng([latitude, longitude]);
     map.setView([latitude, longitude], 4);
     document.querySelector('#lat').textContent = latitude;
     document.querySelector('#lon').textContent = longitude;
+    document.querySelector('#altitude').textContent = altitude.toFixed(1); // toFixed()-method eliminates the number of decimals. 
+    document.querySelector('#velo').textContent = velocity;
+    document.querySelector('#date').textContent = timestamp;
     console.log(latitude);
     console.log(longitude);
 }
